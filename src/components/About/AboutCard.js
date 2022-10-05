e1dedc2a0bb84a64ca96cc765688f78e72e141f3
DerLomo <hardcoreminecraft5@gmail.com>
2022-10-05 22:05:14 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,20 +8,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="green">Lomo </span>
+            from <span className="green"> Valencia, Spain.</span>
+            <br />I am a junior just egresed from Physics at University of Barcelona.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight />Landscape astrophotography
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Lifting iron
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
